8536e892cf4c91d73453abe952a864c5a54df0d0
FWiman <frewim@gmail.com>
2025-11-03 18:56:08 +0100

    OutputStyle.css: changed some styling. OutputPage.js: removed placeholder. OutputPage.jsx: changed some logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import page components
-import HomePage from './pages/home-page/HomePage';
-import ProfilePage from './pages/profile-page/ProfilePage';
-import InputPage from './pages/input-page/InputPage';
-import OutputPage from './pages/output-page/OutputPage';
-import LogRegPage from './pages/log-reg-page/LogRegPage';
-import PrivacyPolicy from './pages/home-page/PrivacyPolicy';
-import './pages/home-page/style/HomePage.css';
-import Cookies from './pages/home-page/Cookies';
+import HomePage from "./pages/home-page/HomePage";
+import ProfilePage from "./pages/profile-page/ProfilePage";
+import InputPage from "./pages/input-page/InputPage";
+import OutputPage from "./pages/output-page/OutputPage";
+import LogRegPage from "./pages/log-reg-page/LogRegPage";
+import PrivacyPolicy from "./pages/home-page/PrivacyPolicy";
+import "./pages/home-page/style/HomePage.css";
+import Cookies from "./pages/home-page/Cookies";
 
 function App() {
   return (
